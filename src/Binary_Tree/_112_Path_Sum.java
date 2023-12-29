c99a1c6a3e9215b5b088cb99b822cd17f461e937
KushagraGupta18 <kushagragpt2003@gmail.com>
2023-12-29 11:07:33 +0530

    commit

---
+++ src/Binary_Tree/_112_Path_Sum.java
@@ -0,0 +1,34 @@
+//leetcode question number --->112
+package Binary_Tree;
+
+public class _112_Path_Sum {
+
+	public class TreeNode {
+		       int val;
+		       TreeNode left;
+		       TreeNode right;
+		       TreeNode() {}
+		       TreeNode(int val) { this.val = val; }
+		       TreeNode(int val, TreeNode left, TreeNode right) {
+		           this.val = val;
+		           this.left = left;
+		           this.right = right;
+		       }
+	}
+	
+	class Solution {
+	    public boolean hasPathSum(TreeNode root, int targetSum) {
+	        if(root==null) {
+	        	return false;
+	        }
+	    	if(root.left==null && root.right==null && targetSum-root.val==0){
+	    		return true;	
+	    	}
+	    	
+	    	boolean leftsum=hasPathSum(root.left, targetSum-root.val);
+	    	boolean rightsum=hasPathSum(root.right, targetSum-root.val);
+		    return leftsum || rightsum;
+	    }
+	}
+	
+}
