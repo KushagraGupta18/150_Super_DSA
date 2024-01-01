41647e98d3f396bfed300a2fa6a963db5bba91b1
KushagraGupta18 <kushagragpt2003@gmail.com>
2024-01-02 01:31:26 +0530

    commit

--- src/Binary_search_tree/_450_Delete_Node_BST.java
+++ src/Binary_search_tree/_450_Delete_Node_BST.java
@@ -20,7 +20,9 @@ public class _450_Delete_Node_BST {
 	
 	class Solution {
 	    public TreeNode deleteNode(TreeNode root, int key) {
-	    	
+	    	if(root==null) {
+	    		return null;
+	    	}
 	    	if(root.val<key) {
 	    		root.right=deleteNode(root.right, key);
 	    	}
@@ -29,9 +31,39 @@ public class _450_Delete_Node_BST {
 	    	}
 	    	//element mil gya isse delete krna hai
 	    	else {
-	    		
+	    		//1 child and 0 child
+	    		if(root.left==null) {
+	    			return root.right;
+	    		}
+	    		else if(root.right==null) {
+	    			return root.left;
+	    		}
+	    		//2 child
+	    		//way 1--->deleted node ki jgh left tree ke maximum ko rkhdo
+	    		//way 2--->deleted node ki jgh right tree ke minimum ko rkhdo
+	    		else {
+	    			int lmax=max(root.left);
+	    			root.left=deleteNode(root.left,lmax);
+	    			root.val=lmax;
+	    		}
 	    	}
+	    	return root;
 	   }
+	   
+	    public int max(TreeNode root) {
+	    	if(root==null) {
+	    		return Integer.MIN_VALUE;
+	    	}
+	    	int r=max(root.right);//right ki call se kaam chl jayega kyuki bst hai
+	    	return Math.max(root.val, r);
+	    }
 	    
+//	    public int min(TreeNode root) {
+//	    	if(root==null) {
+//	    		return Integer.MAX_VALUE;
+//	    	}
+//	    	int r=min(root.left);//left ki call se kaam chl jayega kyuki bst hai
+//	    	return Math.max(root.val, r);
+//	    }
 	}
 }
---
+++ src/Dynamic_Programming/fibbonaccii.java
@@ -0,0 +1,48 @@
+package Dynamic_Programming;
+
+public class fibbonaccii {
+
+	public static void main(String[] args) {
+		//Fibbonacci
+		int n=5;
+	    int[] dp=new int[n+1];
+	    System.out.println(fibb(n));
+		System.out.println(fibbTD(n,dp));
+		System.out.println(fibbBU(n));
+	}
+	
+	
+	private static int fibb(int n) {
+		if(n==0 || n==1) {
+			return n;
+		}
+		int f1=fibb(n-1);
+		int f2=fibb(n-2);
+		return f1+f2; 
+	}
+
+
+	//top down approach
+	public static int fibbTD(int n, int[] dp) {
+		if(n==0 || n==1) {					//fibbTD----> fibonacci top down
+			return n;
+		}
+		if(dp[n]!=0) {				//dp ye hai
+			return dp[n];
+		}
+		int f1=fibbTD(n-1,dp);
+		int f2=fibbTD(n-2, dp);
+		return dp[n]=f1+f2; 		//yaad krna
+		
+	}
+	//bottom up approach (bottom up is more optimised than top down approach)
+	public static int fibbBU(int n) {			//fibbBU---> fibonacci bottom up
+		int[] dp=new int[n+1];
+		dp[0]=0;
+		dp[1]=1;
+		for(int i=2;i<dp.length;i++) {
+			dp[i]=dp[i-1]+dp[i-2];
+		}
+		return dp[n];
+	}
+}
