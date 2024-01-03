95ef9c66a6382104f9d1fa301d2ab53ae98320cb
KushagraGupta18 <kushagragpt2003@gmail.com>
2024-01-03 21:13:35 +0530

    commit

--- src/Dynamic_Programming/_518_Coin_Change.java
+++ src/Dynamic_Programming/_518_Coin_Change.java
@@ -14,7 +14,14 @@ public class _518_Coin_Change {
 			Arrays.fill(a, -1);
 		}
 		System.out.println(CoinChange(coin, amount, 0,dp));
+		
+		for(int[] a:dp) {
+			Arrays.fill(a, -1);
+		}
+		System.out.println(coinChangeBu(coin, amount));
+		
 	}
+	//top down approach
 	public static int CoinChange(int[] coin, int amount, int i,int[][] dp) {
 		if(amount==0) {
 			return 1;
@@ -34,4 +41,24 @@ public class _518_Coin_Change {
 		return dp[amount][i]=inc+exc;
 	}
 
+	
+	//bottom up approach
+	public static int coinChangeBu(int[] coin, int amount) {
+		int[][] dp=new int[amount+1][coin.length+1];
+		for(int i=0;i<dp[0].length;i++) {
+			dp[0][i]=1;
+		}
+		for(int am=1;am<dp.length;am++) {
+			for(int i=1;i<dp[0].length;i++) {
+				int inc=0;
+				int exc=0;
+				if(am>=coin[i-1]) {
+					inc=dp[am-coin[i-1]][i];
+				}
+				exc=dp[am][i-1];
+				dp[am][i]=inc+exc;
+			}
+		}
+		return dp[amount][coin.length];
+	}
 }
