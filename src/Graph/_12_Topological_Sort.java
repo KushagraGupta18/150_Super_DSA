e3dd346607d8f8fdd0b0025127fb7d8ab5b6db84
KushagraGupta18 <kushagragpt2003@gmail.com>
2024-01-11 22:59:37 +0530

    topological

---
+++ src/Graph/_12_Topological_Sort.java
@@ -0,0 +1,18 @@
+package Graph;
+
+import java.util.HashMap;
+
+public class _12_Topological_Sort {
+
+	//Kahn's Algorithm
+		HashMap<Integer, HashMap<Integer, Integer>> map=new HashMap<>();
+		public _12_Topological_Sort(int v) {
+			for(int i=1;i<=v;i++) {
+				map.put(i, new HashMap<>());
+			}
+		}
+		public void AddEdge(int v1 ,int v2) {
+			map.get(v1).put(v2, 0);//v1--->v2 (unweighted)
+			
+		}
+}
